c0d06928c877259578686cc5abd406c921f2507b
Emrullah <emrullahayin@gmail.com>
2019-04-24 22:54:24 +0300

    add the searchForm component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchForm from "./components/SearchForm";
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <SearchForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+
+const styles = {
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 400
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  }
+};
+
+const SearchForm = props => {
+  const { classes } = props;
+  const APP_ID = "1d018658";
+  const APP_KEY = "eefe424911982c21e5f6506847fff4fe";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("counter:", counter);
+  });
+
+  return (
+    <Paper className={classes.root} elevation={1}>
+      <InputBase className={classes.input} placeholder="Search Google Maps" />
+      <IconButton
+        className={classes.iconButton}
+        aria-label="Search"
+        color="primary"
+        onClick={() => setCounter(counter + 1)}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+SearchForm.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SearchForm);
